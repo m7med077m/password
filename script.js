7d6a766871b73ed4dceaf826c8da573165b40ddd
m7med077m <135227322+m7med077m@users.noreply.github.com>
2023-06-01 14:19:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var password = document.getElementById("passwordInput").value;
+    if (password === "18622") {
+        window.location.href = "https://drive.google.com/file/d/1Y13ySPj9RfY8tUo9vgz_9iE30jTeJJkM/view?usp=sharing"; // Replace with your desired URL
+    } else {
+        document.getElementById("result").innerHTML = "Invalid password. Please try again.";
+    }
+});
