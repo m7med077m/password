b5d47299c195141b19c9c630a0e9914821b007ba
m7med077m <135227322+m7med077m@users.noreply.github.com>
2023-06-01 14:39:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById("passwordForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    var password = document.getElementById("passwordInput").value;
+    var password = document.getElementById("الباسورد بتاعنا🙂").value;
     if (password === "18622") {
         window.location.href = "https://drive.google.com/file/d/1Y13ySPj9RfY8tUo9vgz_9iE30jTeJJkM/view?usp=sharing"; // Replace with your desired URL
     } else {
-        document.getElementById("result").innerHTML = "Invalid password. Please try again.";
+        document.getElementById("result").innerHTML = "الباس بتاعناا 🙂";
     }
 });
