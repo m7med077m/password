b8e1e30c436aea753bbe71bd52955c1210c8068b
m7med077m <135227322+m7med077m@users.noreply.github.com>
2023-06-01 15:09:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ document.getElementById("passwordForm").addEventListener("submit", function(even
         window.location.href = "https://drive.google.com/file/d/1Y13ySPj9RfY8tUo9vgz_9iE30jTeJJkM/view?usp=sharing"; // Replace with your desired URL
     } else {
         document.getElementById("result").innerHTML = "الباس بتاعناا 🙂";
+        document.getElementById("passwordInput").value = "";
+
     }
 });
